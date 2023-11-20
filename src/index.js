8da5438addb01f29103c742dd3a3a5b9f84b1653
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-11-20 11:02:47 -0500

    Adding comments to explain my code

--- src/index.js
+++ src/index.js
@@ -16,17 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
    // Use a GET request to fetch the toy data.
   // Create a <div class="card"> for each toy and add it to the #toy-collection div.
 
-// let toysData;
-
 fetch("http://localhost:3000/toys")
 .then(res => res.json())
 .then(toys => {
 
-  // toysData = toys;
-  // console.log(toys);
-
+  // Select the #toy-collection div:
   const toyCollectionDiv = document.querySelector("#toy-collection");
 
+  // Create the cards for each toy in the db:
   toys.forEach(toy => {
     const createCardDiv = document.createElement("div");
     createCardDiv.className = "card";
@@ -42,6 +39,8 @@ fetch("http://localhost:3000/toys")
     createCardDiv.append(toyImg);
 
     const pTag = document.createElement("p");
+
+    // Create function to distinguish singular/plural number of likes:
     function displayNumLikes(num) {
       if (num === 1) {
         pTag.textContent = "1 like";
@@ -49,6 +48,8 @@ fetch("http://localhost:3000/toys")
         pTag.textContent = `${num} likes`;
       }
     } 
+
+    // Call the function with the number of likes from db for each toy:
     displayNumLikes(toy.likes);
     
     createCardDiv.append(pTag);
@@ -59,17 +60,21 @@ fetch("http://localhost:3000/toys")
     buttonTag.textContent = "Like ❤️";
     createCardDiv.append(buttonTag);
 
-    // Create like button functionality
+    // Create like button functionality:
 
+    // Get current number of likes:
     let numOfLikes;
     numOfLikes = toy.likes;
 
-    console.log(`${toy.name} has ${toy.likes} likes.`)
+    // console.log(`${toy.name} has ${toy.likes} likes.`)
 
+    // Add event listener to the button:
     buttonTag.addEventListener("click", event => {
 
+      // Increment numOfLikes by one when the button is clicked: 
       let newNumOfLikes = ++numOfLikes;
 
+      // Use a PATCH request to update the number of likes in the db:
       fetch(`http://localhost:3000/toys/${toy.id}`, {
         method: "PATCH",
         headers:{
@@ -80,6 +85,7 @@ fetch("http://localhost:3000/toys")
         })
       })
 
+      // Execute the displayNumLikes function with the new number to update the text in the p tag:
       displayNumLikes(newNumOfLikes);
     })
 
@@ -89,17 +95,23 @@ fetch("http://localhost:3000/toys")
 
 // Create form functionality
 const form = document.querySelector(".add-toy-form");
+
+// Store the name and image url values in a let variable:
 let newToyName;
 let newToyImg;
 
+// Add event listener to the form submit event:
 form.addEventListener("submit", event => {
   event.preventDefault();
+
+  // Assign the values of newToyName and newToyImg to the "value" inside each of the text input fields:
   newToyName = document.querySelector("#new-toy-name").value;
   newToyImg = document.querySelector("#new-toy-image").value;
 
   console.log(newToyName);
   console.log(newToyImg);
 
+  // POST Request to add new item to the db, interpolating the  value of newtoyName and newToyImg:
   fetch("http://localhost:3000/toys", {
     method: "POST",
     headers:{
