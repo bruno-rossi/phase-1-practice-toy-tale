2b53a84a7da2dfb7e8dd353789ddb9f536a8e078
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-11-20 13:49:23 -0500

    Refactored to use a function to create each card individually, for better efficiency

--- src/index.js
+++ src/index.js
@@ -14,132 +14,131 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
-//    // Use a GET request to fetch the toy data.
-//   // Create a <div class="card"> for each toy and add it to the #toy-collection div.
+  // Use a GET request to fetch the toy data.
+  // Create a <div class="card"> for each toy and add it to the #toy-collection div.
 
-// fetch("http://localhost:3000/toys")
-// .then(res => res.json())
-// .then(toys => {
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(toys => {    
+    // displayToysAsCards(toysData);
+    console.log(toys);
 
-//   toysData = toys;
-  
-//   displayToysAsCards(toysData);
-  
-// });
-// })
-
-// function displayToysAsCards(toys) {
-//   toys.forEach(toy => {
-//     const createCardDiv = document.createElement("div");
-//     const toyNameTag = document.createElement("h2");
-//     const toyImg = document.createElement("img");
-//     const pTag = document.createElement("p");
-
-//     createCardDiv.className = "card";
-//     toyNameTag.textContent = toy.name;
-//     toyImg.src = toy.image;
-//     toyImg.className = "toy-avatar"; 
-  
-//     // Create function to distinguish singular/plural number of likes:
-//     function displayNumLikes(num) {
-//       if (num === 1) {
-//         pTag.textContent = "1 like";
-//       } else {
-//         pTag.textContent = `${num} likes`;
-//       }
-//     } 
-//     // Call the function with the number of likes from db for each toy:
-//     displayNumLikes(toy.likes);
+    toys.forEach(toy => {
+      displayToyCard(toy);
+    });
     
-//     const toyCollectionDiv = document.querySelector("#toy-collection");
+  });
+})
 
-//     createCardDiv.append(toyNameTag);
-//     createCardDiv.append(toyImg);
-//     toyCollectionDiv.append(createCardDiv); 
-//     createCardDiv.append(pTag);
+function displayToyCard(toy) {
+  const createCardDiv = document.createElement("div");
+  const toyNameTag = document.createElement("h2");
+  const toyImg = document.createElement("img");
+  const pTag = document.createElement("p");
+  const buttonTag = document.createElement("button");
+
+  createCardDiv.className = "card";
+  toyNameTag.textContent = toy.name;
+  toyImg.src = toy.image;
+  toyImg.className = "toy-avatar"; 
+  buttonTag.className = "like-btn";
+  buttonTag.id = toy.id;
+  buttonTag.textContent = "Like ❤️";
+
+  const toyCollectionDiv = document.querySelector("#toy-collection");
+
+  createCardDiv.append(toyNameTag);
+  createCardDiv.append(toyImg);
+  createCardDiv.append(pTag);
+  toyCollectionDiv.append(createCardDiv);
+  createCardDiv.append(buttonTag);
   
-//     // Create like button structure and functionality:
-//     const buttonTag = document.createElement("button");
-//     buttonTag.className = "like-btn";
-//     buttonTag.id = toy.id;
-//     buttonTag.textContent = "Like ❤️";
-//     createCardDiv.append(buttonTag);
+  function displayNumLikes(num) {
 
-//     // Get current number of likes:
-//     let numOfLikes;
-//     numOfLikes = toy.likes;
+    if (num === 1) {
+      pTag.textContent = "1 like";
+    } else {
+      pTag.textContent = `${num} likes`;
+    }
+  }
+  displayNumLikes(toy.likes);
+
+  // Create like button functionality:
   
-//     // console.log(`${toy.name} has ${toy.likes} likes.`)
-  
-//     // Add event listener to the button:
-//     buttonTag.addEventListener("click", event => {
-  
-//       // Increment numOfLikes by one when the button is clicked: 
-//       let newNumOfLikes = ++numOfLikes;
-  
-//       // Use a PATCH request to update the number of likes in the db:
-//       fetch(`http://localhost:3000/toys/${toy.id}`, {
-//         method: "PATCH",
-//         headers:{
-//           "Content-Type": "application/json",
-//           Accept: "application/json" },
-//         body: JSON.stringify({
-//           "likes": newNumOfLikes
-//         })
-//       })
-  
-//       // Execute the displayNumLikes function with the new number to update the text in the p tag:
-//       displayNumLikes(newNumOfLikes);
-//     })
-  
-//   });
+  // Get current number of likes:
+  let numOfLikes;
+  numOfLikes = toy.likes;
+      
+  // console.log(`${toy.name} has ${toy.likes} likes.`)
+      
+  // Add event listener to the button:
+  buttonTag.addEventListener("click", event => {
 
-// }
+    // Increment numOfLikes by one when the button is clicked: 
+    let newNumOfLikes = ++numOfLikes;
+        
+    // Use a PATCH request to update the number of likes in the db:
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json" },
+      body: JSON.stringify({
+        "likes": newNumOfLikes
+      })
+    })
 
-// // Create form functionality
-// const form = document.querySelector(".add-toy-form");
+    // Execute the displayNumLikes function with the new number to update the text in the p tag:
+    displayNumLikes(newNumOfLikes);
+  })
 
-// // Store the name and image url values in a let variable:
-// let newToyName;
-// let newToyImg;
+}
 
-// // Add event listener to the form submit event:
-// form.addEventListener("submit", event => {
-//   event.preventDefault();
+// Create form functionality
+const form = document.querySelector(".add-toy-form");
 
-//   // Assign the values of newToyName and newToyImg to the "value" inside each of the text input fields:
-//   newToyName = document.querySelector("#new-toy-name").value;
-//   newToyImg = document.querySelector("#new-toy-image").value;
+// Store the name and image url values in a let variable:
+let newToyName;
+let newToyImg;
 
-//   console.log(newToyName);
-//   console.log(newToyImg);
+// Add event listener to the form submit event:
+form.addEventListener("submit", event => {
+  event.preventDefault();
 
-//   // POST Request to add new item to the db, interpolating the value of newtoyName and newToyImg:
-//   fetch("http://localhost:3000/toys", {
-//     method: "POST",
-//     headers:{
-//       "Content-Type": "application/json",
-//       Accept: "application/json" },
+  // Assign the values of newToyName and newToyImg to the "value" inside each of the text input fields:
+  newToyName = document.querySelector("#new-toy-name").value;
+  newToyImg = document.querySelector("#new-toy-image").value;
 
-//     body: JSON.stringify({
-//       "name": `${newToyName}`,
-//       "image": `${newToyImg}`,
-//       "likes": 0
-//     })
-//   })
-//   .then(res => res.json())
-//   .then(newToy => {
-//     console.log(newToy);
-//     toysData.push({ 
-//       id: newToy.id,
-//       name: newToy.name,
-//       image: newToy.image,
-//       likes: newToy.likes
-//       });
-//     console.log(toysData);
-//     displayToysAsCards(toysData);
-//     debugger
-//     form.reset();
-//   }
-//   )
-// });
+  console.log(newToyName);
+  console.log(newToyImg);
+
+  // POST Request to add new item to the db, interpolating the value of newtoyName and newToyImg:
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json" },
+
+    body: JSON.stringify({
+      "name": `${newToyName}`,
+      "image": `${newToyImg}`,
+      "likes": 0
+    })
+  })
+  .then(res => res.json())
+  .then(newToy => {
+    console.log(newToy);
+    
+    displayToyCard(newToy);
+    // toysData.push({ 
+    //   id: newToy.id,
+    //   name: newToy.name,
+    //   image: newToy.image,
+    //   likes: newToy.likes
+    //   });
+    // console.log(toysData);
+    // displayToysAsCards(toysData);
+    debugger
+    form.reset();
+  })
+});
